f7499abff15fd0baaef82a60cc640771e6fb5af5
sereshenka <sergey.kolesnikov30@yandex.ru>
2016-10-12 23:58:18 +0400

    my version

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,24 @@
 import json
+import os
+import sys
 
 
-def load_data(filepath):
-    pass
-
+def load_json_data(filepath):
+    if not os.path.exists(filepath):
+        print ('Не верный путь до файла\файла не существует,перезапустите программу и введите правильные данные')
+        sys.exit()
+        return None
+    with open(filepath, 'r', encoding = 'utf-8') as file_handler:
+        return json.load(file_handler)
 
 def pretty_print_json(data):
-    pass
-
+    print(json.dumps(data, indent = 4,encure_ascii=False))                
 
 if __name__ == '__main__':
     pass
+
+print('Введите путь до файла(если файл лежит'
+      'в этойже директории,то введите просто имя файла)')
+path = str(input())
+file_json = load_json_data(path)
+pretty_print_json(file_json)
