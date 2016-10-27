0e833f4fdada696f61e5b66dc46d9aa7dcffeec0
sereshenka <sergey.kolesnikov30@yandex.ru>
2016-10-27 17:38:38 +0300

    Сделал рефакторинг,добавил nargs
    Теперь не выводит ошибку,если не задавать аргументов

--- pprint_json.py
+++ pprint_json.py
@@ -12,39 +12,51 @@ def load_json_data(file_path):
         print ('Не верный путь до файла\файла не существует,перезапустите программу и введите правильные данные')
         return None
     else:
-        try:
-            with open(file_path, 'r', encoding = 'utf-8') as file_handler:
-                return json.load(file_handler)
-        except ValueError :
-            print('Это не json файл')
-            return None
-
+        return (open_json(file_path))
+    
+        
+def open_json(file_path):
+    try:
+        with open(file_path, 'r', encoding = 'utf-8') as file_handler:
+            return json.load(file_handler)
+    except ValueError :
+        print('Это не json файл')
+        return None
+    
+    
 def load_win_unicode_console():
     if sys.platform == 'win32':
         import win_unicode_console
         win_unicode_console.enable()
+        
 
-def create_parser():
+def read_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--json', help='Укажите путь к файлу в формате .json')
+    parser.add_argument('--json', help='Укажите путь к файлу в формате .json', nargs = '?')
     file_json = parser.parse_args().json
+    return (existence_of_input(file_json,parser))
+
+
+def existence_of_input(file_json,parser):
     if not file_json:
         parser.print_help()
         return None
     else:
         return (file_json)
-    
+        
+
 
 def pretty_print_json(data):
     try:
         print(json.dumps(data, indent = 4,ensure_ascii = False))
     except UnicodeEncodeError:
         print('Ошибка с кодировками в выводе в консоль')
+        
 
 if __name__ == '__main__':
     load_win_unicode_console()
-    file_path = create_parser()
+    file_path = read_arguments()
     if file_path is not None:
-        open_json = load_json_data(file_path)
-        if open_json is not None:
-            pretty_print_json(open_json)
+        json_file = load_json_data(file_path)
+        if json_file is not None:
+            pretty_print_json(json_file)
